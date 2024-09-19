a4a12a8b94e8305e4089955b14dfe30b6b50237f
Loicarthur <89398456+Loicarthur@users.noreply.github.com>
2024-09-19 14:40:18 +0200

    Résolution des conflits dans models/users.js

--- models/users.js
+++ models/users.js
@@ -1,33 +1,17 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  refreshToken: String,
-  createdAt: {date:Date},
-  updatedAt: {date:Date},
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
-});
+const userSchema = mongoose.Schema(
+  {
+    username: String,
+    email: String,
+    password: String,
+    refreshToken: String,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('users', userSchema);
-
-module.exports = User;
-=======
-import mongoose from 'mongoose'
-
-const userSchema = mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  refreshToken: String,
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
-},{ timestamps: true });
-
-const User = mongoose.model('users', userSchema);
+const User = mongoose.model("users", userSchema);
 
 export default User;
->>>>>>> 6d4e3da6f0c7e01b61604b7a1496d7c95800d1d4
