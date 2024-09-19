ec513dd87a55a22869f9c520a8ecec51ed059632
Loicarthur <89398456+Loicarthur@users.noreply.github.com>
2024-09-19 14:37:56 +0200

    Résolution des conflits dans models/connection.js et models/users.js

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-require("dotenv").config();
-=======
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
->>>>>>> 6d4e3da6f0c7e01b61604b7a1496d7c95800d1d4
 
 const mongoURI = process.env.MONGO_URI;
 
@@ -14,16 +9,8 @@ if (!mongoURI) {
   throw new Error("MONGO_URI environment variable is not defined");
 }
 
-<<<<<<< HEAD
+const connectionString = mongoURI;
 mongoose
-  .connect(mongoURI)
-
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
-module.exports = mongoose;
-=======
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch(error => console.error(error,'Database connection failed'));
->>>>>>> 6d4e3da6f0c7e01b61604b7a1496d7c95800d1d4
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.error(error, "Database connection failed"));
