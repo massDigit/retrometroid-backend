6f1ca153f204603812e1e8da10a26bab4e29289f
Loicarthur <89398456+Loicarthur@users.noreply.github.com>
2024-09-19 14:34:12 +0200

    Résolution des conflits dans models/connection.js

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import createError from "http-errors";
+import express from "express";
+import path, { dirname } from "path";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import http from "http";
+import { fileURLToPath } from "url";
 
-require("./models/connection");
+const filename = fileURLToPath(import.meta.url);
+// const dirname = dirname(filename);
+import "./models/connection.js";
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+import optionsRouter from "./routes/options.js";
+import productsRouter from "./routes/products.js";
+import colorRouter from "./routes/colors.js";
+import accessorieRouter from "./routes/accessories.js";
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
+const app = express();
+const log = morgan("dev");
+const port = process.env.PORT;
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(logger("dev"));
+app.use(log);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
+app.use("/options", optionsRouter);
+app.use("/products", productsRouter);
+app.use("/colors", colorRouter);
+app.use("/accessories", accessorieRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -47,8 +60,6 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
